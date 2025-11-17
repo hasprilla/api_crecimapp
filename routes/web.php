3a100355ba39ac8fcc9a40fe2731c8b73ac302fe
Harvey Asprilla <kikiyam_8@hotmail.com>
2025-11-17 12:23:31 -0500

    Acceso denegado

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
-    echo 'PHP Version: '.phpversion();
-
+    return response()->json([
+        'message' => 'Acceso denegado',
+        'status' => 403, ,
+    ], 403);
 });
