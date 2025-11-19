ae36c9bc41ad11d7d83deeb72a1875efb78189a7
Harvey Asprilla <kikiyam_8@hotmail.com>
2025-11-18 22:01:05 -0500

    Ruta publica

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FileStorageController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['jwt.auth'])->group(function () {
     Route::get('/users/{id}', [UserController::class, 'findById']);
     Route::put('/users/{id}', [UserController::class, 'update']);
-
 });
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
+Route::get('storage/{path}', [FileStorageController::class, 'serveFile'])
+        ->where('path', '.*');
