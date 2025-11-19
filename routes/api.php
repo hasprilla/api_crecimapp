c443d1b14f9ef17cd865357a199f0a41083afd95
Harvey Asprilla <kikiyam_8@hotmail.com>
2025-11-18 20:15:07 -0500

    test

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
@@ -17,13 +19,23 @@ Route::middleware(['jwt.auth'])->group(function () {
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
-
-Route::get('/check-file', function() {
+Route::get('/check-file', function () {
     $filePath = 'users/19/pMV8ZRJNPtg0muUbBDYyKJc69D51qI1ZxHzDvArH.png';
-    
+
     return [
         'exists_in_public' => Storage::disk('public')->exists($filePath),
-        'full_path' => storage_path('app/public/' . $filePath),
-        'files_in_directory' => Storage::disk('public')->files('users/19')
+        'full_path' => storage_path('app/public/'.$filePath),
+        'files_in_directory' => Storage::disk('public')->files('users/19'),
+    ];
+});
+
+Route::get('test-image-url', function () {
+    $user = User::find(5);
+
+    return [
+        'image_path' => $user->image,
+        'image_url' => $user->image_url,
+        'host' => request()->getHttpHost(),
+        'environment' => app()->environment(),
     ];
 });
